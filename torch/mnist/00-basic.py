55517d7ef59728648efcf278b6387fb247562e7a
kimdoyun0208 <kdyandy0208@gmail.com>
2025-05-27 14:15:37 +0900

    change model layers

--- torch/mnist/00-basic.py
+++ torch/mnist/00-basic.py
@@ -37,16 +37,13 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(28*28, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 10)
+        self.fc2 = nn.Linear(128, 10)
     
     def forward(self, x):
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
-        x = nn.functional.relu(x)
+        x = nn.functional.softmax(x)
         x = self.fc2(x)
-        x = nn.functional.relu(x)
-        x = self.fc3(x)
         return x
     
 model = Net().to(DEVICE)
